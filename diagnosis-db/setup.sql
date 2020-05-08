6d90a0d9c9893f015f2cbd4398ae6b2d201431ca
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-05-07 22:24:07 -0700

    add indexes to diagnosis db

--- diagnosis-db/setup.sql
+++ diagnosis-db/setup.sql
@@ -5,6 +5,8 @@ CREATE TABLE IF NOT EXISTS reported_keys (
     uploaded_at  TIMESTAMP NOT NULL DEFAULT NOW(),
     PRIMARY KEY(TEK, ENIN)
 );
+CREATE INDEX enin_idx ON reported_keys(ENIN);
+CREATE INDEX hak_idx ON reported_keys(HAK);
 
 CREATE TABLE IF NOT EXISTS health_authorities (
     HAK          BYTEA PRIMARY KEY,

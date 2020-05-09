64f408ab7abd035ef9f38b711da351255155e90f
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-05-09 00:12:54 -0700

    make sure generation script puts bindings in simulate/

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python -m grpc_tools.protoc -Iproto --python_out=diagnosis-api/. --grpc_python_out=diagnosis-api/. proto/db19.proto
-python -m grpc_tools.protoc -Iproto --python_out=usage/. --grpc_python_out=usage/. proto/db19.proto
+python -m grpc_tools.protoc -Iproto --python_out=simulate/. --grpc_python_out=simulate/. proto/db19.proto
